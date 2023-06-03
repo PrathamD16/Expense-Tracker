5a37014d04d1abebd2aa436ad8f8cf174489d59c
PrathamD16 <pratham.doke.1602@gmail.com>
2023-06-03 15:35:05 +0530

    First Commit

--- components/TranList.js
+++ components/TranList.js
@@ -3,12 +3,12 @@ import React from "react";
 function TranList({ transaction, amount }) {
   return (
     <div className="mx-6 mt-2 flex items-center">
-      <div className="bg-purple-500 p-2 text-white rounded-sm text-xl hover:bg-purple-900 cursor-pointer">
-        <p>X</p>
+      <div className="bg-gray-200 p-2 rounded-sm text-xl hover:bg-purple-900 hover:text-white cursor-pointerx">
+        <button>X</button>
       </div>
-      <div className={`${amount < 0 ? `bg-red-600`:`bg-green-600`} flex p-2 rounded-sm text-xl text-white justify-between px-3 flex-1 flex-grow`}>
-        <p>{transaction}</p>
-        <p>₹{amount}</p>
+      <div className={`${amount < 0 ? `bg-gradient-to-r from-gray-200 to-red-600`:`bg-gradient-to-r from-gray-200 to-green-600`} flex p-2 rounded-sm text-xl text-white justify-between px-3 flex-1 flex-grow`}>
+        <p className="text-black font-medium">{transaction}</p>
+        <p className="text-white">₹{Math.abs(amount)}</p>
       </div>
     </div>
   );
